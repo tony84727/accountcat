9f6a7b90ee2197a1068a34d1ad8326d94f14cbf9
Tony Duan <tony84727@gmail.com>
2025-07-31 20:48:56 +0800

    ui: extract LinkTab component

--- ui/src/TodoList.tsx
+++ ui/src/TodoList.tsx
@@ -1,19 +1,13 @@
-import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
-import { Link } from "react-router";
+import LinkTab from "./LinkTab";
 import { useRouteMatch } from "./muiutils";
 export default function TodoList() {
 	const routeMatch = useRouteMatch(["/todo/history", "/todo"]);
 	const currentTab = routeMatch?.pattern?.path;
 	return (
 		<Tabs value={currentTab}>
-			<Tab
-				label="History"
-				component={Link}
-				to="/todo/history"
-				value="/todo/history"
-			/>
-			<Tab label="Todo" component={Link} to="/todo" value="/todo" />
+			<LinkTab label="History" to="/todo/history" value="/todo/history" />
+			<LinkTab label="Todo" to="/todo" value="/todo" />
 		</Tabs>
 	);
 }
