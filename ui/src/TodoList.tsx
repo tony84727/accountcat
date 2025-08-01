7e1776f01039f9e7cf3b031997a746f1fd0fc00e
Tony Duan <tony84727@gmail.com>
2025-08-01 21:41:59 +0800

    ui: task list

--- ui/src/TodoList.tsx
+++ ui/src/TodoList.tsx
@@ -1,21 +1,52 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Tabs from "@mui/material/Tabs";
-import LinkTab from "./LinkTab";
-import { useRouteMatchCurrentTab } from "./muiutils";
+import Divider from "@mui/material/Divider";
+import Input from "@mui/material/Input";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { type FormEvent, useCallback, useState } from "react";
 export default function TodoList() {
-	const currentTab = useRouteMatchCurrentTab(["/todo/history", "/todo"]);
+	const [tasks, setTasks] = useState<string[]>([]);
+	const [taskName, setTaskName] = useState<string>("");
+	const onTaskNameInput = useCallback((event: FormEvent<HTMLInputElement>) => {
+		setTaskName((event.target as HTMLInputElement).value);
+	}, []);
+	const onAddTask = useCallback(() => {
+		if (!taskName) {
+			return;
+		}
+		setTasks((x) => [...x, taskName]);
+		setTaskName("");
+	}, [taskName]);
 	return (
 		<Container>
-			<Grid container>
-				<Grid>
-					<Tabs value={currentTab} orientation="vertical">
-						<LinkTab label="Todo" to="/todo" value="/todo" />
-						<LinkTab label="History" to="/todo/history" value="/todo/history" />
-					</Tabs>
-				</Grid>
-				<Grid>Items</Grid>
-			</Grid>
+			<Box>
+				<Input
+					type="text"
+					placeholder="任務名稱"
+					value={taskName}
+					onInput={onTaskNameInput}
+				/>
+				<Button onClick={onAddTask}>新增任務</Button>
+			</Box>
+
+			<List>
+				{tasks.map((x, i) => (
+					<>
+						<ListItem key={`${i}${x}`}>
+							<ListItemIcon>
+								<Checkbox />
+							</ListItemIcon>
+							<ListItemText primary={x} secondary="date" />
+						</ListItem>
+						<Divider component="li" />
+					</>
+				))}
+			</List>
 		</Container>
 	);
 }
