fcf37c0f61a1b545768f5b0fea0891032cbbfe09
Tony Duan <tony84727@gmail.com>
2025-08-29 17:50:31 +0800

    server: add graceful shutdown
    add graceful shutdown per axum's example
    
    See: https://github.com/tokio-rs/axum/blob/94db5273a5514fed80b2c0841c055143ebd8b105/examples/graceful-shutdown/src/main.rs

--- src/server.rs
+++ src/server.rs
@@ -4,6 +4,7 @@ use axum::Router;
 use clap::Parser;
 use http::header;
 use sqlx::PgPool;
+use tokio::signal;
 use tonic_web::GrpcWebLayer;
 use tower::ServiceBuilder;
 use tower_http::set_header::SetResponseHeaderLayer;
@@ -99,5 +100,32 @@ pub async fn main(arg: &ServerArg) {
         .fallback_service(asset_service);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
