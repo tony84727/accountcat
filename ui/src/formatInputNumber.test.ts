fd65b9de050d06ddeef8a52de69b4712d3ba6f97
Tony Duan <tony84727@gmail.com>
2025-08-29 13:37:26 +0800

    ui: accounting: format user number input

---
+++ ui/src/formatInputNumber.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "@rstest/core";
+import formatInputNumber from "./formatInputNumber";
+
+describe("formatInputNumber", () => {
+	test("should allow input 0", () => {
+		expect(formatInputNumber("0")).toBe("0");
+	});
+	test("should format non-number input to 0", () => {
+		expect(formatInputNumber("hello")).toBe("0");
+	});
+	test("should not change integer input", () => {
+		expect(formatInputNumber("10")).toBe("10");
+	});
+	test("should remove leading zeros", () => {
+		expect(formatInputNumber("010")).toBe("10");
+		expect(formatInputNumber("010.0")).toBe("10.0");
+	});
+	test("should allow on-going decimal input", () => {
+		expect(formatInputNumber("10.")).toBe("10.");
+	});
+	test("should remove charaters other that digits and deciaml seperator", () => {
+		expect(formatInputNumber("10,0")).toBe("100");
+	});
+});
---
+++ ui/src/formatInputNumber.ts
@@ -0,0 +1,7 @@
+export default function formatInputNumber(input: string): string {
+	const matches = input.replace(/[^\d.]/, "").match(/\d+.?(\d+)?/);
+	if (!matches) {
+		return "0";
+	}
+	return matches[0].replace(/^0+(?=[^0]+)/, "");
+}
