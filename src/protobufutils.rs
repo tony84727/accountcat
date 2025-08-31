117aad39b579b4c21f5aa3190506bededf92df08
Tony Duan <tony84727@gmail.com>
2025-08-31 20:43:10 +0800

    implement protobuf to OffsetDateTime conversion

--- src/protobufutils.rs
+++ src/protobufutils.rs
@@ -1,9 +1,15 @@
 use prost_types::Timestamp;
-use sqlx::types::time::OffsetDateTime;
+use time::OffsetDateTime;
 
 pub fn to_proto_timestamp(datetime: OffsetDateTime) -> Timestamp {
     Timestamp {
         seconds: datetime.unix_timestamp(),
         nanos: datetime.nanosecond() as i32,
     }
+}
+
+pub fn from_proto_timestamp(
+    timestamp: Timestamp,
+) -> Result<OffsetDateTime, time::error::ComponentRange> {
+    OffsetDateTime::from_unix_timestamp(timestamp.seconds)
 }
