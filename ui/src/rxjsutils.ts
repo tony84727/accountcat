f069c36028d0c32da4640b62cec6aa288d5546ee
Tony Duan <tony84727@gmail.com>
2025-09-23 22:57:02 +0800

    ui: remove unused rxjs helpers

--- ui/src/rxjsutils.ts
+++ ui/src/rxjsutils.ts
@@ -1,33 +1,5 @@
-import {
-	type Dispatch,
-	type SetStateAction,
-	useEffect,
-	useMemo,
-	useState,
-} from "react";
-import { map, type Observable, Subject } from "rxjs";
-import type { AjaxResponse } from "rxjs/ajax";
-
-export function useObservable<T>(observable: Observable<T>) {
-	const [state, setState] = useState<T>();
-	useEffect(() => {
-		const subscription = observable.subscribe((x) => setState(x));
-		return () => subscription.unsubscribe();
-	}, [observable]);
-	return state;
-}
-
-export function unwrapResponse<T>() {
-	return map(({ response }: AjaxResponse<T>) => response);
-}
-
-export function useSubject<T = undefined>(): [
-	Observable<T>,
-	(event: T) => void,
-] {
-	const subject = useMemo(() => new Subject<T>(), []);
-	return [subject, (x: T) => subject.next(x)];
-}
+import type { Dispatch, SetStateAction } from "react";
+import { type Observable, Subject } from "rxjs";
 
 export function createNotifier(
 	updateDispatch: Dispatch<SetStateAction<(() => void) | undefined>>,
